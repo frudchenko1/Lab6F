f716081c02c0728e1d286ee87e555a59c5c4a802
frudchenko1 <frudchenko@ufl.edu>
2023-10-25 20:07:26 -0400

    This is my first addition to the repo

---
+++ main.py
@@ -0,0 +1,26 @@
+def encode_pass(password):
+    res = ""
+
+    for i in password:
+        res += str(int(i) + 3)
+
+    return res
+
+
+def main():
+    while True:
+        print('''\nMenu
+-------------
+1. Encode
+2. Decode
+3. Quit
+        ''')
+        menu_option = int(input('Please enter an option: '))
+        if menu_option == 1:
+            password = input('Please enter your password to encode: ')
+            encode_pass(password)
+            print("Your password has been encoded and stored!")
+
+
+if __name__ == '__main__':
+    main()
